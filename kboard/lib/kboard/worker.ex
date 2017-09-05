a2522a5f3cff53e987d6556b556e80cd33acdf06
Max Strange <maxfieldstrange@gmail.com>
2017-09-04 17:50:26 -0700

    WIP kboard

--- kboard/lib/kboard/worker.ex
+++ kboard/lib/kboard/worker.ex
@@ -19,11 +19,50 @@ defmodule Kboard.Worker do
   end
 
   defp parse_log_file(path) do
-    File.read(path)
+    File.read!(path)
+    |> String.split("\n")
+    |> Enum.map(fn(s)-> parse_line(s) end)
+    |> Enum.filter(fn(ls)-> ls != [] end)
+    |> convert_to_map()
+    |> IO.inspect()
   end
 
-  defp print_images(_blah) do
-    Plot.plot([[1, 2, 3]])
-    Plot.show()
+  defp parse_line(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(fn(s)-> String.trim(s) end)
+    |> Enum.map(fn(s)-> convert(s) end)
+    |> Enum.filter(fn(s)-> s != "" end)
+  end
+
+  defp convert(str) do
+    cond do
+      str == "" -> ""
+      String.match?(str, ~r/^(-)+ [0-9]+ (-)+$/) -> ""
+      String.match?(str, ~r/^[0-9]+/) -> String.to_float(str)
+      String.match?(str, ~r/^[a-zA-Z]+/) -> str
+    end
+  end
+
+  defp convert_to_map([names|nums]) do
+    # This returns a map: %{name: [values], another_name: [values]}
+    rearranged = for {name, index} <- Enum.zip(names, 0..Enum.count(names)) do
+      values = for num_list <- nums do
+        Enum.at(num_list, index)
+      end
+    end
+    Enum.zip(names, rearranged)
+    |> Enum.into(%{})
+  end
+
+  defp print_images(dict) do
+    # dict is a map: %{name: [values], another_name: [values]}
+    for {name, values} <- dict do
+      Plot.ylim([[0, 1]])
+      Plot.title(name)
+      Plot.plot([values])
+      Plot.savefig(["./assets/static/images/" <> name <> ".png"])
+      Plot.cla()
+    end
   end
 end
