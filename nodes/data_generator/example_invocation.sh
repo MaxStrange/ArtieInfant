cb375c5a30c3e0f9214363d7bd1a19384a31771e
MaxStrange <maxfieldstrange@gmail.com>
2018-02-11 14:57:37 -0800

    Add example invocation for data_generator

---
+++ nodes/data_generator/example_invocation.sh
@@ -0,0 +1,1 @@
+python data_generator.py ~/Dropbox/thesis/data/2018/ --producer_topics audiosegments --producer_configs bootstrap-server=10.0.0.11:9092 --loglevel DEBUG --slice_length 0.5 --remove_silence
