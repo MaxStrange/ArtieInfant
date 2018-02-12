95b2ca04c6b79bc8a8c67c10052a43d6dcee99d5
Max Strange <maxfieldstrange@gmail.com>
2018-02-11 22:28:43 -0800

    Resampler

--- nodes/resampler/resampler.py
+++ nodes/resampler/resampler.py
@@ -13,11 +13,14 @@ def resample(seg):
     def resample(self, sample_rate_Hz=None, sample_width=None, channels=None, console_output=False):
     """
     # Resample to 16bit @ 16kHz mono
+    logging.info("Resampling a sample named " + str(seg.name))
     return seg.resample(sample_rate_Hz=16000, sample_width=2, channels=1)
 
 if __name__ == "__main__":
     consumer_names, producer_names, consumer_configs, producer_configs = myargparse.parse_args()
-    logging.info("CONSUMING FROM:", consumer_names, "PRODUCING TO:", producer_names)
+    logging.basicConfig(level="DEBUG")
+    logging.info("CONSUMING FROM: " + str(consumer_names))
+    logging.info("PRODUCING TO: " + str(producer_names))
 
     mykafka.init_consumer(**consumer_configs)
     mykafka.init_producer(**producer_configs)
