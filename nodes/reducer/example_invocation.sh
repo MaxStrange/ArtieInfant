49e9fab4b9df69576bef280eca706f1111b55b7e
MaxStrange <maxfieldstrange@gmail.com>
2018-02-14 22:15:01 -0800

    Reducer invocation

--- nodes/reducer/example_invocation.sh
+++ nodes/reducer/example_invocation.sh
@@ -1,1 +1,2 @@
-python reducer.py --consumer_configs bootstrap-server=$KAFKA_SERVER --consumer_topics resampled
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+python $SCRIPT_DIR/reducer.py --consumer_configs bootstrap-server=$KAFKA_SERVER fetch-max-bytes=268435456 max-partition-fetch-bytes=268435456 retry-backoff-ms=2000 max-in-flight-requests-per-connection=1 --consumer_topics resampled
