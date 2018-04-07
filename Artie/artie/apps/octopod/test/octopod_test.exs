18d6c90784aabca0b8243099e8f7152536a84995
Max Strange <max.strange@synapse.com>
2018-04-07 16:30:18 -0700

    Octopod: Add test for passing in file contents

--- Artie/artie/apps/octopod/test/octopod_test.exs
+++ Artie/artie/apps/octopod/test/octopod_test.exs
@@ -2,8 +2,8 @@ defmodule OctopodTest do
   use ExUnit.Case
   doctest Octopod
 
-  @pyoptions Application.get_env(:octopod, :erlport_options)
   @priv_path Application.app_dir(:octopod, "priv")
+  @pyoptions [{:cd, to_charlist(@priv_path)} | Application.get_env(:octopod, :erlport_options)]
 
   test "Initializes Python Process" do
     # Start the python instance using erlport
@@ -17,7 +17,7 @@ defmodule OctopodTest do
   test "Runs Simple Python Script" do
     # Start a python process and have it execute some code loaded from a script
 
-    {:ok, python} = Octopod.start_pyprocess([{:cd, to_charlist(@priv_path)} | @pyoptions])
+    {:ok, python} = Octopod.start_pyprocess(@pyoptions)
     {:ok, 18} = Octopod.execute_script(python, :test_simple_script)
     :ok = Octopod.stop_pyprocess(python)
   end
@@ -25,8 +25,16 @@ defmodule OctopodTest do
   test "Runs Simple Python Script with Args" do
     # Start a python process and pass it some values to add together
 
-    {:ok, python} = Octopod.start_pyprocess([{:cd, to_charlist(@priv_path)} | @pyoptions])
+    {:ok, python} = Octopod.start_pyprocess(@pyoptions)
     {:ok, 312} = Octopod.execute_script(python, :test_add_nums, [300, 12])
+    :ok = Octopod.stop_pyprocess(python)
+  end
+
+  test "Runs Simple Python Script with File as Arg" do
+    # Start a python process and pass a serialized WAV file to it - it will save it under a different name
+    {:ok, python} = Octopod.start_pyprocess(@pyoptions)
+    wav_contents = Path.join(@priv_path, "furelise.wav") |> File.read!()
+    {:ok, 'saved!'} = Octopod.execute_script(python, :save_file, [wav_contents])
     :ok = Octopod.stop_pyprocess(python)
   end
 end
