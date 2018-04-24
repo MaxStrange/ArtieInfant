158ffefc7e2968fc1caac20f3a6ce7d830df034c
Max Strange <max.strange@synapse.com>
2018-04-24 13:01:37 -0700

    Update README and add opts

--- Artie/artie/apps/test_at_scale/lib/test_at_scale.ex
+++ Artie/artie/apps/test_at_scale/lib/test_at_scale.ex
@@ -1,17 +1,4 @@
 defmodule TestAtScale do
   @moduledoc """
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TestAtScale.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- Artie/artie/apps/test_at_scale/test/test_at_scale_test.exs
+++ Artie/artie/apps/test_at_scale/test/test_at_scale_test.exs
@@ -2,18 +2,50 @@ defmodule TestAtScaleTest do
   use ExUnit.Case
   doctest TestAtScale
 
-  test "greets the world" do
-    assert TestAtScale.hello() == :world
+  @hostname :"foo@localhost"
+
+  setup_all do
+    {:ok, _pid} = Node.start(@hostname)
+    :ok
   end
 
-  test "Can Spawn Processes on Same Host" do
-    hostname = :"foo@localhost"
-    {:ok, _pid} = Node.start(hostname)
-    mypid = self()
-    _pypid0 = Node.spawn_link(hostname, fn -> Pyctopod.start(:pyctotest_consume, nil, mypid) end)
-    Process.sleep(3_000)  # Spawning is asynchronous, so we may publish before we have subscribed
-    _pypid1 = Node.spawn_link(hostname, fn -> Pyctopod.start(:pyctotest_pub_one_msg) end)
+  setup do
+    Node.stop()
+    {:ok, _pid} = Node.start(@hostname)
+    :ok
+  end
 
-    assert_receive({:pyctotest_consume, :test, "This is a Test FROM PYTHON!"}, 30_000)
+  test "Can Spawn A and B on Same Host" do
+    mypid = self()
+    _pypid0 = Node.spawn_link(@hostname, fn -> Pyctopod.start(:pyctotest_consume, nil, mypid) end)
+    Process.sleep(3_000)  # Spawning is asynchronous, so we may publish before we have subscribed
+    _pypid1 = Node.spawn_link(@hostname, fn -> Pyctopod.start(:pyctotest_pub_one_msg) end)
+
+    assert_receive({:pyctotest_consume, :test, "This is a Test FROM PYTHON!"}, 15_000)
+
+    Node.stop()
+  end
+
+  test "Can Spawn A and B on Different Hosts" do
+    hostA = @hostname
+    hostB = :"B@archbox"
+    true = Node.connect(hostB)
+    pid = Node.spawn(hostB, fn -> IO.puts "HEY" end)
+    assert(is_pid(pid) == true)
+    Node.stop()
+  end
+
+  test "Can Spawn Pyctopod A and B on Different Hosts" do
+    hostA = @hostname
+    hostB = :"B@10.1.59.187"
+
+    mypid = self()
+    Node.spawn_link(hostA, fn -> Pyctopod.start(:pyctotest_consume, nil, mypid) end)
+    Process.sleep(5_000)
+    Node.spawn_link(hostB, fn -> Pyctopod.start(:pyctotest_pub_one_msg) end)
+    
+    assert_receive({:pyctotest_consume, :test, "This is a Test FROM PYTHON!"}, 15_000)
+
+    Node.stop()
   end
 end
