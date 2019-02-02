f5a97c96ec8983f798b9ac00534db7c6c09b5c06
Max Strange <maxfieldstrange@gmail.com>
2019-02-02 13:00:57 -0800

    Add tune_spectrogram script

--- scripts/tune_spectrogram/tune_spectrogram.py
+++ scripts/tune_spectrogram/tune_spectrogram.py
@@ -9,27 +9,36 @@ useful features from them easier.
 import audiosegment as asg
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Need path to test audio file")
+        exit(1)
+    elif not os.path.isfile(sys.argv[1]):
+        print("Need a valid path")
+        exit(2)
+
     #################################################################
     # Here are the configurations that you should tune to your liking
     #################################################################
     # Resampling
-    sample_rate_hz  = 32000.0    # 32kHz sample rate
+    sample_rate_hz  = 16000.0    # 16kHz sample rate
     bytewidth       = 2          # 16-bit samples
     nchannels       = 1          # mono
 
     # Spectrogram
-    duration_s      = 0.1          # Duration of each complete spectrogram
+    duration_s      = 0.5        # Duration of each complete spectrogram
     window_length_s = 0.03       # How long each FFT is
-    overlap         = 0.5        # How much each FFT overlaps with each other one
+    overlap         = 0.2        # How much each FFT overlaps with each other one
 
     # Track
-    first_start_s   = 100        # Where in the track should we start grabbing spectrograms?
+    first_start_s   = 16          # Where in the track should we start grabbing spectrograms?
     #################################################################
 
     # Load the audio file into an AudioSegment
-    seg = asg.from_file("testsegment.wav")
+    seg = asg.from_file(sys.argv[1])
     seg = seg.resample(sample_rate_Hz=sample_rate_hz, sample_width=bytewidth, channels=nchannels)
 
     nspectrograms = 4
@@ -38,7 +47,7 @@ if __name__ == "__main__":
         start_s = idx * duration_s + first_start_s
         frequencies, times, amplitudes = seg.spectrogram(start_s, duration_s, window_length_s=window_length_s, overlap=overlap)
 
-        # Log the amplitudes to make them stand out more
+        # Log the amplitudes to help with contrast
         amplitudes = 10 * np.log10(amplitudes + 1e-9)
 
         # Plot into a subplot
@@ -46,5 +55,5 @@ if __name__ == "__main__":
         plt.subplot(subplotidx)
         plt.pcolormesh(times, frequencies, amplitudes)
 
-    # Now finally show the spectrograms
+    ## Now finally show the spectrograms
     plt.show()
