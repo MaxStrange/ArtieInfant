662970c46655642f5dc16e9107d5f6eadb987ce2
Max Strange <maxfieldstrange@gmail.com>
2019-04-14 14:41:12 -0700

    Update config files and add run-experiments.sh

---
+++ Artie/run-experiments.sh
@@ -0,0 +1,27 @@
+# -------- Short time RMS genetic experiments --------
+#python3 main.py experiment/configfiles/genetic2/1.cfg --pretrain-synth --loglevel info
+#python3 main.py experiment/configfiles/genetic2/2.cfg --pretrain-synth --loglevel info
+#python3 main.py experiment/configfiles/genetic2/3.cfg --pretrain-synth --loglevel info
+#python3 main.py experiment/configfiles/genetic2/4.cfg --pretrain-synth --loglevel info
+
+# -------- Short time XCOR genetic experiments ------
+python3 main.py experiment/configfiles/genetic2/5.cfg --pretrain-synth --train-synth --loglevel info
+
+# --------- Long time baseline genetic experiments --
+
+# --------- Short time baseline genetic experiments -
+
+# --------- Short time vanilla latent dims rerun ----
+
+# --------- Short time Euclid genetic experiments ---
+
+# --------- Long time vanilla latent dims 30 epochs -
+
+# --------- Short time vanilla latent dims 30 epochs
+
+# --------- Long time Overfitting -------------------
+
+# --------- Short time Underfitting -----------------
+
+# --------- Variational Loss Function ---------------
+
